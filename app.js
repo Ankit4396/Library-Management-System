ef9a054b0ccceedbd76b9dbadc74877a2ca30106
Ankit Jaiswal <49371017+Ankit4396@users.noreply.github.com>
2020-11-02 20:00:36 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
-var express = require('express');
+const express = require('express');
+const bcrypt=require('bcrypt');
 
+const passport=require('passport');
 const bodyParser = require("body-parser");
-var mongoose=require('mongoose');
+const mongoose=require('mongoose');
 const libRoutes=require('./routes/home');
 const mainRoutes=require('./routes/mainpage');
-var config=require('./config/database');
+//const config=require('./config/database');
 const path=require('path');
-mongoose.connect(config.database);
-var db=mongoose.connection;
-db.on('error',console.error.bind(console,'connection error:'));
-db.once('open',function(){
-console.log('Connected to MongoDB');
-});
-var app=express();
+const app=express();
+const session=require('express-session');
+require('./config/passport')(passport);
+app.set('views',"views");
+app.set('view engine','ejs');
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+  app.use(session({
+      secret:'secret',
+      resave:true,
+      saveUninitialized:true
+  }));
+  //passport middleware
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+//const Schema = mongoose.Schema;
+
+
+app.use('/getBooks',(req,res,next)=>{
+    const books=booksModel.find({},(err,data)=>{
+        console.log(data);
+        res.send(data);
+    });
+});
 app.use(libRoutes);
 app.use(mainRoutes);
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
+
+
+
 
 app.use(express.static(path.join(__dirname,'public')));
 
-
-var port=3002;
-app.listen(port,function(){
-    console.log('server'+port);
+const port=3002;
+mongoose.connect("mongodb://localhost:27017/lms",{ useNewUrlParser: true , useUnifiedTopology: true} ).then((result)=>{
+    app.listen(port,function(){
+        console.log('server'+port);
+    });    
+console.log("Database connected succesfully");
 })
+.catch((err)=>{
+    console.log("can't connect to database",err);
+});
+
